a9830333c727cd8686d7904e76217064d2cd93a5
DanMayuri <dpalomamm001@gmail.com>
2025-05-02 10:09:11 -0500

    avance2

--- 01_basico/02_tipos.py
+++ 01_basico/02_tipos.py
@@ -1,1 +1,32 @@
 #Python tiene varios tipos de datos
+#int, float, coaplex, str, bool, NoneType, list,tuple,dict,range,set...
+
+
+
+"""
+hikaaaaaaaaaaaaaaaaaaa
+"""
+#print("int:")
+#print (type(10))
+#print(type(0))
+#print(type(-5))
+#print(type(3456789778976544798778987))
+
+print("float:")
+print(type(3.24))
+print(type(1e3))
+
+num1=10*10*10
+print(num1)
+print(type(num1))
+
+
+num2 = 1e3
+print(num2)
+print(type(num2))
+
+
+
+
+#ausencia de valor
+print("NoneType")
