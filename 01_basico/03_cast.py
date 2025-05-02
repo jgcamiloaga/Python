43d14d1ce37309b19f3ed44fb05a1176ad1318bb
DanMayuri <dpalomamm001@gmail.com>
2025-05-02 17:07:59 -0500

    avance3-convertir tipos de datos

--- 01_basico/03_cast.py
+++ 01_basico/03_cast.py
@@ -1,2 +1,19 @@
 #03 Casting de tipos
 # Python permite convertir un tipo de dato a otro, esto se llama casting.
+print("conversacion de tipos")
+print(type(int("100")))
+#print("100"+ 2)-------error (DIFERENTES TIPOS DE VARIABLES)
+#print("100"+ str(2)) #1002
+#print(round(3.5))--- 3 pq es el entero mas cercano
+#si fuera 3.51--- seria 4
+#print(type(float("3.14.16")))
+#print(int("3.1416"))----REDONDEA AL PAR MAS CERCANO
+
+#print(bool(3))
+#print(bool(0))
+#print(bool(-1))
+
+#print(bool(0))---FLSE-----numero distintos + o -  TRUE
+#print(bool(""))---FALSE
+#print(bool(" "))---TRUE
+#print(bool("False"))
