872cc31b40946db6eccd7b2626432cb134e639d1
jgcamiloaga <jcamiloc148@gmail.com>
2025-05-07 17:54:55 -0500

    Ejercicios parte 2

--- 02_flow_control/02_booleans.py
+++ 02_flow_control/02_booleans.py
@@ -12,5 +12,67 @@ print("5 >3:", 5 > 3) #true
 print("5 <= 3:", 5 <= 3) #true
 
 
+#Ejercicio 1: Sistema de calificación avanzado
+#Escribe un programa que le pida al usuario su nota (de 0 a 100). Luego:
+#Si la nota es mayor o igual a 90, imprimir "Excelente".
+#Si está entre 80 y 89, imprimir "Muy bien".
+#Si está entre 70 y 79, imprimir "Bien".
+#Si está entre 60 y 69, imprimir "Suficiente".
+#Si está entre 0 y 59, imprimir "Insuficiente".
+#Si la nota no está en ese rango, imprimir "Nota no válida".
+
+
+
+
+
+#Ejercicio 2: Calculadora de tarifas
+#Crea un programa que:
+#Pida al usuario su edad y el día de la semana.
+#Si el usuario tiene menos de 12 años o más de 65, el boleto cuesta $5.
+#Si es fin de semana (sábado o domingo), cuesta $8.
+#Si no, cuesta $10.
+#Asegúrate de validar correctamente los datos ingresados.
+
+
+
+
+
+
+#Ejercicio 3: Verificador de acceso
+#Pide al usuario que introduzca:
+#Su nombre de usuario.
+#Su contraseña.
+#Su rol (admin, editor, lector).
+#Si el nombre de usuario y la contraseña son correctos (puedes fijar unos valores), y además:
+#Si es admin, imprimir "Acceso total".
+#Si es editor, imprimir "Acceso limitado".
+#Si es lector, imprimir "Solo lectura".
+#Si la contraseña o el usuario son incorrectos, imprimir "Acceso denegado".
+
+
+
+
+
+
+#Ejercicio 4: Adivina el número
+#El usuario debe ingresar un número del 1 al 100.
+#Si el número es divisible por 3 y por 5, imprime “FizzBuzz”.
+#Si solo es divisible por 3, imprime “Fizz”.
+#Si solo es divisible por 5, imprime “Buzz”.
+#Si no es divisible ni por 3 ni por 5, imprime el número.
+#Si el número no está entre 1 y 100, imprime “Número fuera de rango”.
+
+
+
+
+
+
+#Ejercicio 5: Evaluador de año bisiesto
+#Solicita al usuario un año y determina si es bisiesto:
+#Un año es bisiesto si es divisible por 4 y no por 100, o si es divisible por 400.
+#Usa if, elif, else para tomar la decisión.
+#Imprime si el año es o no bisiesto.
+
+
 
 
