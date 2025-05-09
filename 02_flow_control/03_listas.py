08a11071679f47d16d8c650367331c200e945c6c
DanMayuri <dpalomamm001@gmail.com>
2025-05-09 10:37:08 -0500

    listas

--- 02_flow_control/03_listas.py
+++ 02_flow_control/03_listas.py
@@ -4,15 +4,61 @@
 # Pueden contener elementos de diferentes tipos
 ###
 
+print("\n crear listas")
+lista1 = [1,2,3,4,5] #Lista de enteros
+lista2 = ["manzana","peras","platanos"]#lista de cadenas
+lista3: list [int| str |float |bool] = [1,"hola", 3.24,True] #lista de tipos mixtos
+lista_vacia =[]
+lista_de_listas =[(1,2) , [(3,4)]]
+matrix =[(1,2)],[(2,3)],[("calcetin",5)]
 
 
+print(lista1)
+print(lista2)
+print(lista3)
+print(lista_vacia)
+print(lista_de_listas)
+print(matrix)
 
 
+#Acceso a elementos por indice
+print("\nAcceso a elementos por inidice")
+print(lista2[0])
+print(lista2[1])
+print(lista2[-1])
+print(lista2[-2])
+#0-1-2....
+#-3,-2-1
+
+print(lista_de_listas [1][0])
+
+#slicing (rebanado) de listas
+lista1 = [1,2,3,4,5]
+
+print(lista1 [1:4]) #[2,3,4]
+print(lista1[:3]) # [1,2,3]
+print(lista1[3:]) #[4,5]
+print(lista1[:]) #[1,2,,4,5]
+
+#Hay mas magia
+lista1 = [1,2,3,4,5,6,7,8]
+print(lista1[::2])#[1.4.7] para devolver indices pares
+print(lista1[::-1])#[8-7-6-5-4-3-2-1] para devolver indices impares
+#print(lista1[desde:hasta:paso])
+
+#MODIFICAR UNA LISTA
+lista1[0] = 20
+print(lista1)
+#forma mas larga y menos eficiente
+lista1 =lista1 + [4,5,6]
+print(lista1)
+
+#forma mas corta
+lista1 += [7,8,9]
 
 
-
-
-
+#recuperar longitud de una lista
+print("Longitud de la lista", len(lista1))
 
 ###
 # EJERCICOS
@@ -22,8 +68,9 @@
 # Dada la siguiente lista:
 # mensaje = ["C", "o", "d", "i", "g", "o", " ", "s", "e", "c", "r", "e", "t", "o"]
 # Utilizando slicing y concatenación, crea una nueva lista que contenga solo el mensaje "secreto".
-
-
+mensaje = ["C", "o", "d", "i", "g", "o", " ", "s", "e", "c", "r", "e", "t", "o"]
+lista = mensaje[7:]
+print(lista)
 
 
 
@@ -32,6 +79,11 @@
 # numeros = [10, 20, 30, 40, 50]
 # Intercambia la primera y la última posición utilizando solo asignación por índice.
 
+numeros = [10, 20, 30, 40, 50]
+print(numeros)
+numeros [0] = 50
+numeros [-1] = 10
+print(numeros)
 
 
 
