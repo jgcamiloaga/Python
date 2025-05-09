99332e8ec8bcaf9f2fa0afc80236fe8007b73f89
DanMayuri <dpalomamm001@gmail.com>
2025-05-09 09:41:57 -0500

    vacio

--- 02_flow_control/02_booleans.py
+++ 02_flow_control/02_booleans.py
@@ -164,3 +164,10 @@ if (año %4 ==0 and año %100 != 0) or(año %400 == 0):
     print( f"{año} es bisiesto")
 else:
     print("no es un año bisiesto")
+    #termino
+
+    
+    #DIFERENCIAS DEL GIT RESERT
+    #git reset --soft HEAD~(n° de commit)
+    #git reset --hard (id)
+    #git push origin main --force #hard
