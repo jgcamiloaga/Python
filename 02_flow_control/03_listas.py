75af0fe7f34106b95ee3e03e066bc8845e53f883
DanMayuri <dpalomamm001@gmail.com>
2025-05-14 17:27:09 -0500

    termine los ejercicios de listas

--- 02_flow_control/03_listas.py
+++ 02_flow_control/03_listas.py
@@ -23,7 +23,7 @@ print(matrix)
 
 #Acceso a elementos por indice
 print("\nAcceso a elementos por inidice")
-print(lista2[0])
+print(lista2[0]) #Se imprime la primera de la lista2
 print(lista2[1])
 print(lista2[-1])
 print(lista2[-2])
@@ -93,10 +93,14 @@ print(numeros)
 # pan = ["pan arriba"]
 # ingredientes = ["jamón", "queso", "tomate"]
 # pan_abajo = ["pan abajo"]
-# Crea una lista llamada sandwich que contenga el pan de arriba, los ingredientes y el pan de abajo, en ese orden.
+# Crea una lista llamada sandwich que contenga el pan de arriba,
+#  los ingredientes y el pan de abajo, en ese orden.
 
-
-
+pan = ["pan arriba"]
+ingredientes = ["jamón", "queso", "tomate"]
+pan_abajo = ["pan abajo"]
+sandwich = pan+ ingredientes + pan_abajo
+print(sandwich)
 
 
 # Ejercicio 4: Duplicando la lista
@@ -105,15 +109,20 @@ print(numeros)
 # Crea una nueva lista que contenga los elementos de la lista original duplicados.
 # Ejemplo: [1, 2, 3] -> [1, 2, 3, 1, 2, 3]
 
-
+lista = [1, 2, 3]
+lista_duplicada = lista*2# lista +lista
+print(lista_duplicada)
 
 
 
 # Ejercicio 5: Extrayendo el centro
-# Dada una lista con un número impar de elementos, extrae el elemento que se encuentra en el centro de la lista utilizando slicing.
+# Dada una lista con un número impar de elementos, extrae el elemento 
+# que se encuentra en el centro de la lista utilizando slicing.
 # Ejemplo: lista = [10, 20, 30, 40, 50] -> El centro es 30
 
-
+lista = [10, 20, 30, 40, 50,60,70]
+centro= len(lista) // 2
+print(lista[centro])
 
 
 
@@ -121,6 +130,7 @@ print(numeros)
 # Dada una lista, invierte solo la primera mitad de la lista (utilizando slicing y concatenación).
 # Ejemplo: lista = [1, 2, 3, 4, 5, 6] -> Resultado: [3, 2, 1, 4, 5, 6]
 
-
-
-
+lista = [1, 2, 3, 4, 5, 6]
+centro= len(lista)//2
+lista_invertida = lista[:centro][::-1] + lista[centro:]
+print(lista_invertida)
