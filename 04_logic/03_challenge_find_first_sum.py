da5771282cb3771a57726ba419a8fb4d60189030
DanMayuri <dpalomamm001@gmail.com>
2025-06-18 17:24:42 -0500

    03_ejercicos

--- 04_logic/03_challenge_find_first_sum.py
+++ 04_logic/03_challenge_find_first_sum.py
@@ -1,8 +1,23 @@
 """
-Dado un array de números y un número goal, encuentra los dos primeros números del array que sumen el número goal y devuelve sus índices. Si no existe tal combinación, devuelve None.
+Dado un array de números y un número goal, encuentra los dos primeros números del 
+array que sumen el número goal y devuelve sus índices. Si no existe tal combinación, devuelve None.
 
 nums = [4, 5, 6, 2]
 goal = 8
 
 find_first_sum(nums, goal)  # [2, 3]
 """
+
+nums = [6, 5, 2, 3]
+goal = 11
+
+def find_first_sum (nums,goal):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] + nums[j]== goal:
+                print(f"Los numeros que suman goal son los siguientes : {nums[i]}, {nums[j]}")
+                return [i,j]
+    return None
+
+print(find_first_sum(nums,goal))
+
--- 04_logic/04_dictionaries.py
+++ 04_logic/04_dictionaries.py
@@ -3,3 +3,61 @@
 # Los diccionarios son colecciones de pares clave-valor.
 # Sirven para almacenar datos relacionados.
 ###
+
+#Ejemplo tipico de diccionario
+persona= {
+    "nombre":"Dana",
+    "edad":"20",
+    "Es_estudiante": True,
+    "calificaciones":[7,8,9],
+    "social": {
+        "twitter":"danam20",
+        "instagram": "danamayuri",
+        "facebook":"dana20"
+    }
+    }
+
+
+
+#para acceder a los valores
+print(persona["nombre"])
+print(persona["calificaciones"][2])
+print(persona["socials"]["twitter"])
+
+#Cambiar valores al acceder
+persona["nombre"]="Paloma"
+persona["calificaciones"][2]=10
+
+#eliminar completamente la propiedad
+del persona["edad"]
+print(persona)
+
+es_estudiante =persona.pop("es_estudiante")
+print(f"es_estudiante: (es_estudiante)")
+print(persona)
+
+#sobreescribir un diccionario con otro diccionario
+a = {"name":"dana", "age":20}
+b= {"name":"Paloma", "es_estudiante":True }
+
+a.update(b)
+print(a)
+
+#Comprobar si existe una propiedad
+print("name" in persona )#False
+print("nombre" in persona )#true
+
+#Obtener todas las clave
+print("\nkeys:")
+print(persona.keys())
+
+#Obtener todos los valores
+print("\nvalues:")
+print(persona.values())
+
+#Obtener tanto clave como valor
+print("\nitems:")
+print(persona.items())
+
+for key, value in persona.items():
+    print(f"{key}:{value}")
